12823124418649f7a5f43b04027680f0fe5cc8c5
ctghadiri <ctghadiri@gmail.com>
2019-03-28 16:15:49 -0700

    add requirements for express and route files

--- server.js
+++ server.js
@@ -5,10 +5,15 @@
 
 // establish requirements for modules/files
     // express
-    // path
+var express = require('express');
+var app = express();
+
     // routing files
         // api
+require('./routing/apiRoutes')(app);
         // html
+require('./routing/htmlRoutes')(app);
+
 
 // establish connection
 // setup express to handle data with urlencoded
