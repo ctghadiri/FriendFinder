f3f47fad02ddfb4fe82d836ef09c6b89930b6e8f
ctghadiri <ctghadiri@gmail.com>
2019-03-28 16:10:15 -0700

    pseudocode for server.js and move folders into app

---
+++ server.js
@@ -0,0 +1,15 @@
+// Application
+    // Takes in data from user via form
+        // returns match of another individual
+    // Shows all saved individuals
+
+// establish requirements for modules/files
+    // express
+    // path
+    // routing files
+        // api
+        // html
+
+// establish connection
+// setup express to handle data with urlencoded
+// add listener
