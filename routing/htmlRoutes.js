038a66ccd925d33eeaf027aec07cbd5fabb04f2a
ctghadiri <ctghadiri@gmail.com>
2019-03-29 19:19:36 -0700

    add path to home and survey

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -4,5 +4,9 @@ module.exports = function(app){
 
     app.get('/home', function(req, res){
         res.sendFile(path.join(__dirname, '../app/public/home.html'));
-    })
-}
+    });
+
+    app.get('/survey', function(req, res){
+        res.sendFile(path.join(__dirname, '../app/public/survey.html'));
+    });
+};
