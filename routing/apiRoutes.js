2e4118a3dde89f28ba11a89858b422ef55546161
ctghadiri <ctghadiri@gmail.com>
2019-03-29 20:01:13 -0700

    us get to grab data and post for gathering survey data and pushing data

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,12 @@
-var friendsData = require('../app/data/friends.js');
+var friendsData = require('../app/data/friends');
 
 module.exports = function (app){
-    
+    app.get('../app/data/friends', function (req, res){
+        res.json(friendsData);
+    });
+
+    app.post('/survey', function (req, res){
+        friendsData.push(req.body);
+        res.json(friendsData);
+    })
 }
