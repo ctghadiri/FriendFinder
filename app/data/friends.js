f63ea6ee791b785dad0cead0ddbd0e56472a4717
ctghadiri <ctghadiri@gmail.com>
2019-04-01 14:57:11 -0700

    add more data for link in friends.js and change route name in apiroutes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,18 @@ var friendArray = [
     {
         name: 'Link',
         image: 'https://pbs.twimg.com/media/Cdrft_rXIAAQJCq.jpg',
-        
+        scores: [
+            4,
+            3,
+            5,
+            1,
+            2,
+            3,
+            4,
+            5,
+            3,
+            2
+        ]
 
     }
 ];
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,11 +1,11 @@
 var friendsData = require('../app/data/friends');
 
 module.exports = function (app){
-    app.get('/survey', function (req, res){
+    app.get('/api/friends', function (req, res){
         res.json(friendsData);
     });
 
-    app.post('/survey', function (req, res){
+    app.post('/api/friends', function (req, res){
         friendsData.push(req.body);
         res.json(friendsData);
     });
