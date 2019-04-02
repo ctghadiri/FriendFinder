922edc10da94e43ccd05df5943ff8222a038c1ca
ctghadiri <ctghadiri@gmail.com>
2019-04-02 15:38:02 -0700

    add friend data and modal/functions for finding match

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,38 @@ var friendArray = [
             2
         ]
 
+    },
+    {
+        name: 'Donkey Kong',
+        image: 'https://cdnb.artstation.com/p/assets/images/images/003/616/927/20161005001335/micro_square/joshua-baldi-donkey-kongr.jpg?1475644416',
+        scores: [
+            2,
+            4,
+            3,
+            1,
+            5,
+            3,
+            2,
+            5,
+            5,
+            3
+        ]
+    },
+    {
+        name: 'Mario',
+        image: 'https://i.ytimg.com/vi/UrnzuAdB_us/maxresdefault.jpg',
+        scores: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
     }
 ];
 module.exports = friendArray;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,6 +7,37 @@ module.exports = function (app){
 
     app.post('/api/friends', function (req, res){
         friendsData.push(req.body);
-        res.json(friendsData);
+
+        var differences = [];
+
+        for(var j = 0; j < req.body.scores.length; j++){ 
+            differences.push(arrDifference(req.body.scores, friendArray[j].scores));
+        
+        };
+
+        // for(var k = 0; k < differences.length; k++){
+        //     if(differences[k] === Math.min(differences)){
+        //         return friendsData[k];
+        //     }
+        // }
+        friendsData[differences.indexOf(Math.min(...differences))]
+
+
+        function arrDifference(arr1, arr2){
+            var diffArr = [];
+            for( var i = 0; i < arr1.length; i++){
+              diffArr.push(Math.abs(parseInt(arr1[i]) - parseInt(arr2[i])));
+            };
+    
+            console.log(diffArr)
+    
+            return diffArr.reduce(sum);
+        }
+    
+        function sum(total, num){
+            return total+num;
+        }
+        res.json(friendsData[differences.indexOf(Math.min(...differences))]
+        );
     });
 }
