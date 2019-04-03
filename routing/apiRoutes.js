4557e1f6055f4388f032bfaa78141ecabc39343f
ctghadiri <ctghadiri@gmail.com>
2019-04-02 20:00:33 -0700

    change id in html and adjust add variable for survey data

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,43 +1,41 @@
 var friendsData = require('../app/data/friends');
-
+console.log("friendsdata length:" + friendsData.length)
 module.exports = function (app){
     app.get('/api/friends', function (req, res){
         res.json(friendsData);
     });
 
     app.post('/api/friends', function (req, res){
-        friendsData.push(req.body);
+        var newFriend = req.body
+        console.log("this is newfriend" + newFriend)
+        // res.json(friendsData);
 
         var differences = [];
 
-        for(var j = 0; j < req.body.scores.length; j++){ 
-            differences.push(arrDifference(req.body.scores, friendArray[j].scores));
-        
+        for(var j = 0; j < friendsData.length; j++){ 
+            differences.push(arrDifference(newFriend.scores, friendsData[j].scores));
+            console.log(newFriend.scores + 'hi')
         };
 
-        // for(var k = 0; k < differences.length; k++){
-        //     if(differences[k] === Math.min(differences)){
-        //         return friendsData[k];
-        //     }
-        // }
-        friendsData[differences.indexOf(Math.min(...differences))]
-
-
+        
+        
         function arrDifference(arr1, arr2){
             var diffArr = [];
             for( var i = 0; i < arr1.length; i++){
-              diffArr.push(Math.abs(parseInt(arr1[i]) - parseInt(arr2[i])));
+                diffArr.push(Math.abs(parseInt(arr1[i]) - parseInt(arr2[i])));
             };
-    
+            
             console.log(diffArr)
-    
+            
             return diffArr.reduce(sum);
         }
-    
+        
         function sum(total, num){
             return total+num;
         }
-        res.json(friendsData[differences.indexOf(Math.min(...differences))]
-        );
+        var closestMatch = friendsData[differences.indexOf(Math.min(...differences))];
+        friendsData.push(req.body);
+
+        res.json(closestMatch);
     });
 }
