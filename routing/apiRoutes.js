32e44db56f99320d762f2a8c7536175f9d9682cb
ctghadiri <ctghadiri@gmail.com>
2019-04-02 20:44:26 -0700

    remove console.logs and add validate response

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,5 +1,4 @@
 var friendsData = require('../app/data/friends');
-console.log("friendsdata length:" + friendsData.length)
 module.exports = function (app){
     app.get('/api/friends', function (req, res){
         res.json(friendsData);
@@ -7,14 +6,11 @@ module.exports = function (app){
 
     app.post('/api/friends', function (req, res){
         var newFriend = req.body
-        console.log("this is newfriend" + newFriend)
-        // res.json(friendsData);
 
         var differences = [];
 
         for(var j = 0; j < friendsData.length; j++){ 
             differences.push(arrDifference(newFriend.scores, friendsData[j].scores));
-            console.log(newFriend.scores + 'hi')
         };
 
         
@@ -23,10 +19,7 @@ module.exports = function (app){
             var diffArr = [];
             for( var i = 0; i < arr1.length; i++){
                 diffArr.push(Math.abs(parseInt(arr1[i]) - parseInt(arr2[i])));
-            };
-            
-            console.log(diffArr)
-            
+            };            
             return diffArr.reduce(sum);
         }
         
